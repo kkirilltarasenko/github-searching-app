70cdb1b5b1deba00585ca35df365b017e07ca7fe
whereareyo <75016013+kkirilltarasenko@users.noreply.github.com>
2023-08-06 10:03:13 +0300

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 // Data for requests
-export const GITHUB_TOKEN: string = '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';
+export const GITHUB_TOKEN: string = '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';
 export const BASE_URL: string = 'https://api.github.com/graphql';
 
 // Local storage keys
